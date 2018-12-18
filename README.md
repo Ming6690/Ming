ffe70b789609ffcb1ac840352a9e6c9d42bffcf0
Ming6690 <13126066713@163.com>
2018-12-18 20:34:43 +0800

    项目第一天打卡

--- README.md
+++ README.md
@@ -6,3 +6,22 @@
 1. git add .
 2. git commit -m "提交信息"
 3. git push
+
+
+## 购物车制作
+ + 先把 扩展图标的 css 样式拷贝到项目中,然后在 main.js 中导入
+ + 拷贝 扩展字体库 ttf 文件到项目中
+ + 为购物车图标添加 "mui-icon mui-icon-extra mui-icon-extra-cart" 样式
+
+## 设置路由高亮
+  + 路由选择时，有一个class类的属性为 router-link-active,可以通过类名设置 CSS 样式实现高亮（不推荐）
+  + 使用 MUI 提供的 mui-active 高亮属性替换router-link-active
+  + 在 router.js 下 路由匹配规则 routes:[] 同级目录下 添加 linkActiveClass: 'mui-active' 实现替换 
+
+## 制作首页轮播图布局
+
+## 加载首页轮播图数据
+  1. 获取数据，使用 vue-resource
+  2. 使用 vue-resource 的 this.$http.get 获取数据
+  3. 获取到的数据，要保存到 data 身上
+  4. 使用 v-for 循环渲染每个 item 项
--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
 // 入口文件
 import Vue from 'vue'
 
+// 1.1 导入路由对象
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+// 导入自己的独立的路由模块 router.js
+import router from './router.js'
+
+// 2.1 导入 vue-resource
+import VueResource from  'vue-resource'
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
+
 // 导入 MUI 样式   
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 // 按需导入 Mint-UI 中的组件
-import { Header } from 'mint-ui'
+import { Header, Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 // 导入 app 组件
 import app from './App.vue'
 
 var vm = new Vue({
   el: '#app',
-  render: c => c(app)
+  render: c => c(app),
+  router // 挂载路由对象到 VM 实例上
 })
